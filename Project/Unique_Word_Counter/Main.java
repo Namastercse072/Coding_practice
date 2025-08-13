aaab8ef5538e80a491bd24fa5b141baa79c3c4c1
NamNgo <nam,bkumaster@gmail.com>
2025-08-13 23:04:48 +0700

    Add andvanced statistics feature -clean code

--- Project/Unique_Word_Counter/Main.java
+++ Project/Unique_Word_Counter/Main.java
@@ -1,4 +1,3 @@
-// import Coding_practice.Project.Unique_Word_Counter;
 import java.util.*;
 
 public class Main {
@@ -6,30 +5,36 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         
-        String[] sentences = text.split("\\.");
+        String[] sentences = text.split("\\s+");
         String[][] textArray = new String[sentences.length][];
         // Split each sentence into words and store in the 2D array
         for (int i = 0; i < sentences.length; i++) {
             textArray[i] = sentences[i].trim().split(" ");
-            // System.out.println("Sentence " + (i + 1) + ": " + Arrays.toString(textArray[i]));
         }
-        System.out.println("Word count: ");
+       
         HashMap<String, Integer> wordCount = new HashMap<>();
         for (int i = 0; i < textArray.length; i++) {
             for (int j = 0; j < textArray[i].length; j++) {
                 // Convert each word to lowercase and remove punctuation
                 String word = textArray[i][j];
-                String cleanedWord = word.replaceAll("[^a-zA-Z]", "").toLowerCase();    
-                // System.out.println("Word: " + cleanedWord);
-               
+                String cleanedWord = word.replaceAll("[^a-zA-Z]", "").toLowerCase();     
                 if (!cleanedWord.isEmpty()) {
                     wordCount.put(cleanedWord, wordCount.getOrDefault(cleanedWord, 0) + 1);
                 }
+                
             } 
         }
+
+        System.out.println("Total words: "+ sentences.length);
+        System.out.println("Unique words: "+ wordCount.size());
+        System.out.println("Word statistics:");
+
         for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
                 // Print each unique word and its count
-            System.out.println(entry.getKey() + ": " + entry.getValue());     
+            double percentage = (entry.getValue() * 100.0) / sentences.length;
+            System.out.print(entry.getKey() + ": " + entry.getValue()+" ");
+            System.out.printf("(%.2f%%)\n", percentage);
+     
         }
     }
 }
