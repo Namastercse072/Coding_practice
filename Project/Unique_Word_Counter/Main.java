e2fcd8ce641a5b844c9f3bd5c21b075ad3042859
NamNgo <nam,bkumaster@gmail.com>
2025-08-13 22:14:59 +0700

    relocate Hashmap - fix bug unique cnt

--- Project/Unique_Word_Counter/Main.java
+++ Project/Unique_Word_Counter/Main.java
@@ -14,21 +14,22 @@ public class Main {
             // System.out.println("Sentence " + (i + 1) + ": " + Arrays.toString(textArray[i]));
         }
         System.out.println("Word count: ");
+        HashMap<String, Integer> wordCount = new HashMap<>();
         for (int i = 0; i < textArray.length; i++) {
             for (int j = 0; j < textArray[i].length; j++) {
                 // Convert each word to lowercase and remove punctuation
-                HashMap<String, Integer> wordCount = new HashMap<>();
                 String word = textArray[i][j];
                 String cleanedWord = word.replaceAll("[^a-zA-Z]", "").toLowerCase();    
-                if (!word.isEmpty()) {
-                    // Print the unique word
-                    wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
-                    for (String key : wordCount.keySet()) {
-                        System.out.println(key + ": " + wordCount.get(key));
-                    }
-    
+                // System.out.println("Word: " + cleanedWord);
+               
+                if (!cleanedWord.isEmpty()) {
+                    wordCount.put(cleanedWord, wordCount.getOrDefault(cleanedWord, 0) + 1);
                 }
-            }
+            } 
+        }
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+                // Print each unique word and its count
+            System.out.println(entry.getKey() + ": " + entry.getValue());     
         }
     }
 }
